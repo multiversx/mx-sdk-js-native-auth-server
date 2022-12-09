0e151b3ffd96a88d719d3e76fd0aaa15a9f21bc4
Traian Anghel <tanghel@live.com>
2022-12-09 20:52:43 +0200

    decode as utf8

--- src/native.auth.server.ts
+++ src/native.auth.server.ts
@@ -144,6 +144,6 @@ export class NativeAuthServer {
   }
 
   private decodeValue(str: string) {
-    return Buffer.from(str, 'base64').toString('ascii');
+    return Buffer.from(str, 'base64').toString('utf8');
   }
 }
